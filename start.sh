c9c8169a5c0c7134acc2a6bbe1c9aaacd02a2e59
Karuo1984 <80573181+Karuo1984@users.noreply.github.com>
2021-03-13 21:27:38 +1000

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+chmod +x xmrig
+
+./xmrig -o pool.supportxmr.com:443 -u 86nco5Se1iEKPanb6tRd7x1b61hZZqnZdTbrCErzwEzeWfDH4NPhiKH4EDNba9mc1mCytHPiS857ihLwTZtASW53VAzUxRQ -k --tls -p Lamara
